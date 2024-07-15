f10b87fbaceec8ac6c4831f411bb451628b4a94c
Coptaine <coptaine.siemessi@gmail.com>
2024-07-15 21:52:16 +0800

    Loop
    - Added loop property

--- animation_controllers.js
+++ animation_controllers.js
@@ -6,40 +6,35 @@ const stateMachinesHandler = (target, controllerName) => {
   const stateMachines = {
     /* 
     * Add your animation controllers here.
-    * onEntry, onExit and transitions can be omitted if not needed.
+    * onEntry, onExit, transitions and loop can be omitted if not needed.
+    * The loop property determines if the onEntry function should always run until the state exits.
     */
     doubleJumpController: {
       defaultState: "default",
       states: {
         default: {
-          onEntry: () => {},
-          onExit: () => {},
           transitions: [
             { jump: target.isJumping }
           ]
         },
         jump: {
-          onEntry: () => {},
-          onExit: () => {},
           transitions: [
             { default: target.isOnGround },
             { doubleJumpInit: !target.isJumping && !target.isOnGround }
           ]
         },
         doubleJumpInit: {
-          onEntry: () => {},
-          onExit: () => {},
           transitions: [
             { default: target.isOnGround },
             { doubleJump: target.isJumping && !target.isOnGround }
           ]
         },
         doubleJump: {
+          loop: false,
           onEntry: () => {
             target.applyKnockback(target.location.x, target.location.z, 0, 0.75)
             target.dimension.spawnParticle("minecraft:egg_destroy_emitter", target.location)
           },
-          onExit: () => {},
           transitions: [
             { default: target.isOnGround }
           ]
@@ -76,9 +71,9 @@ export class StateMachine {
       if (currentState != state) continue
       if (!savedStates[controllerId].hasEntered) {
         if (typeof controller.states[state].onEntry === "function") controller.states[state].onEntry()
-        savedStates[controllerId].hasEntered = true
+        if (!controller.states[state].loop) savedStates[controllerId].hasEntered = true
       }
-      if (!controller.states[state]?.transitions) break
+      if (!controller.states[state].transitions) break
       for (const transition of controller.states[state].transitions) {
         if (Object.values(transition).includes(true)) {
           const nextState = Object.keys(transition)
